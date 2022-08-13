b19fe96e4086a7c7d90cee6790c428447a8b61e9
FungluiKoo <fenglei.gu@nyu.edu>
2022-08-13 12:49:21 -0400

    Create 340-longest-substring-with-at-most-k-distinct-characters.cpp

---
+++ 340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -0,0 +1,24 @@
+#include <bitset>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstringKDistinct(string s, int k){
+        if(k<=0 || s.length()==0){return 0;}
+        vector<short> cnt(128,0);
+        bitset<128> flag;
+        int result = 0;
+        for(int left = 0, right = 0; right<s.length(); ++right){
+            if(++cnt[s[right]]>0){flag.set(s[right]);}
+            while(left<right && flag.count()>k){
+                if(--cnt[s[left]]==0){flag.reset(s[left]);}
+                ++left;
+            }
+            if(right-left+1>result){result=right-left+1;}
+        }
+        return result;
+    }
