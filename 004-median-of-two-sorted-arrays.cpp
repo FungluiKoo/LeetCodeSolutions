09142d0547afc04f13e792e01c5d69025253224b
FungluiKoo <fenglei.gu@nyu.edu>
2022-08-16 00:28:15 -0400

    Update 004-median-of-two-sorted-arrays.cpp

--- 004-median-of-two-sorted-arrays.cpp
+++ 004-median-of-two-sorted-arrays.cpp
@@ -1,48 +1,57 @@
+#include <algorithm>
+#include <iostream>
 #include <vector>
 
+using namespace std;
+
 class Solution {
-private:
-    // Interval: [Start, End)
-    double getMedian(std::vector<int>& arr, size_t start, size_t end) {
-        // REQUIRES: 0<=start<=end<=arr.size()
-        auto n = end - start;
-        if (n==0) {return 0;}
-        return (n % 2) ? arr[start+n/2] : (arr[start+n/2]+arr[start+n/2-1])/2.0;
-    }
-
-    size_t updateStart(size_t start, size_t end) {
-        // REQUIRES: start<=end
-        return (start+end)/2;
-    }
-
-    size_t updateEnd(size_t start, size_t end) {
-        // REQUIRES: start<=end
-        return (start+end)/2;
-    }
-
 public:
-    double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
-        size_t s1 = 0, e1 = nums1.size();
-        size_t s2 = 0, e2 = nums2.size();
-        size_t total = nums1.size() + nums2.size();
-        auto med1 = getMedian(nums1, s1, e2);
-        auto med2 = getMedian(nums2, s2, e2);
-        if(nums1.empty()){return med2;}
-        if(nums2.empty()){return med1;}
-
-        while (s1 + s2 != (total-1)/2) {
-            if (med1 < med2) { // med 1 is too small
-                s1 = updateStart(s1, e1);
-                e2 = updateEnd(s2, e2);
-            }else if (med1 > med2){ // med 1 is too large
-                e1 = updateEnd(s1, e1);
-                s2 = updateStart(s2, e2);
-            }else{ // med1 == med2
-                
-            }
-        }
+    int getk(vector<int>& nums1, vector<int>& nums2, int k){
+        int n1 = nums1.size(), n2=nums2.size();
+        if(n1==0){return nums2[k-1];}
+        if(n2==0){return nums1[k-1];}
+        // if(nums1[n1-1]<=nums2[0]){
+        //     return (k<=n1) ? nums1[k-1] : nums2[k-n1-1]; 
+        // }        
+        if(upper_bound(nums1.cbegin(),nums1.cend(),nums2[0])-nums1.cbegin()>=k){return nums1[k-1];}
+        if(lower_bound(nums2.cbegin(),nums2.cend(),nums1[n1-1])-nums2.cbegin()<k-n1){return nums2[k-n1-1];}
+        // if(nums2[n2-1]<=nums1[0]){
+        //     return (k<=n2) ? nums2[k-1] : nums1[k-n2-1];
+        // }
+        if(upper_bound(nums2.cbegin(),nums2.cend(),nums1[0])-nums2.cbegin()>=k){return nums2[k-1];}
+        if(lower_bound(nums1.cbegin(),nums1.cend(),nums2[n2-1])-nums1.cbegin()<k-n2){return nums1[k-n2-1];}
         
-
-
+        // now the answer must be in the overlapped part
+        int left=max(1,k-n2), right=min(k-1,n1);
+        int x = (left+right)/2;
+        while(left<right){
+            if(nums2[k-x-1]>nums1[x]){ // x too small
+                left = (left==right-1) ? (left+1) : x;
+            }else if(k-x<n2 && nums1[x-1]>nums2[k-x]){ // x too large
+                right = x;
+            }else{
+                break;
+            }
+            x = (left+right)/2;
+        }
+        return max(nums1[x-1],nums2[k-x-1]);
+    }
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size()+nums2.size();
+        if(n==0){ // both empty
+            return 0.0;
+        }else if(n % 2){ // odd
+            return double(getk(nums1,nums2,(n+1)/2));
+        }else{ // even
+            return (getk(nums1,nums2,n/2)+getk(nums1,nums2,n/2+1))/2.0;
+        }
     }
 };
+
+int main(){
+    Solution sol;
+    vector<int> a1 = {1,2};
+    vector<int> a2 = {1,2};
+    cout << sol.getk(a1,a2,2) << endl << sol.getk(a1,a2,3) << endl;
+    return 0;
+}
