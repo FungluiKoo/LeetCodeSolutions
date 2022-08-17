46ef8658c6e4c961efd5f5d8759341c998b74b45
FungluiKoo <fenglei.gu@nyu.edu>
2022-08-17 12:10:35 -0400

    Update 075-sort-colors.cpp

--- 075-sort-colors.cpp
+++ 075-sort-colors.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
     void sortColors(vector<int>& nums) {
         vector<int> cnt(3);
@@ -13,6 +13,28 @@ public:
         for(int x=0; x<3; ++x){
             for(int i=0; i<cnt[x]; ++i){
                 *(it++) = x;
+            }
+        }
+    }
+};
+
+class Solution2 {
+public:
+    void sortColors(vector<int>& nums) {
+        int left=0, mid=0, right = nums.size()-1;
+        while(mid<=right){
+            switch (nums[mid]){
+            case 0:
+                swap(nums[left++],nums[mid++]);
+                break;
+            case 1:
+                mid++;
+                break;
+            case 2:
+                swap(nums[mid],nums[right--]);
+                break;
+            default:
+                break;
             }
         }
     }
