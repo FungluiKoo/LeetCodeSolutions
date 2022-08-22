92045b7adcf467eae299b83ee8c38d1d40622f7b
FungluiKoo <fenglei.gu@nyu.edu>
2022-08-21 23:37:45 -0400

    Update 005-longest-palindromic-substring.cpp

--- 005-longest-palindromic-substring.cpp
+++ 005-longest-palindromic-substring.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-class Solution {
+class Solution1 { // Dynamic Programming
 public:
     string longestPalindrome(string s) {
         int N = s.length();
@@ -27,3 +28,45 @@ public:
         return result;
     }
 };
+
+class Solution2 { // Manacher's Algorithm
+private:
+    string str = "#";
+    int n = 1;
+    vector<int> arm;
+public:
+    string longestPalindrome(string s) { 
+        if(s.length()<2){return s;}
+        for(auto ch:s){
+            str += ch;
+            str += '#';
+        }
+        n = str.length();
+        arm.resize(n, 0);
+        int center = 0, right = 0, maxarm = 0, max_i = 0;
+        for(int i=1; i<n-1; ++i){
+            if(i<right){
+                arm[i] = min(arm[2*center-i], right-i);
+            }
+            while(i-arm[i]-1>=0 && i+arm[i]+1<n\
+                 && str[i-arm[i]-1]==str[i+arm[i]+1]){
+                ++arm[i];
+            }
+            if(i+arm[i]>right){
+                center = i;
+                right = i+arm[i];
+            }
+            if(arm[i]>maxarm){
+                maxarm = arm[i];
+                max_i = i;
+            }
+        }
+        return s.substr((max_i-maxarm)/2, maxarm);
+    }
+};
+
+int main(){
+    Solution2 sol;
+    cout << sol.longestPalindrome("babad") << endl;
+    return 0;
+}
