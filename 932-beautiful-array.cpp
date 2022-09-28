efc355393f7ccad00d1fb08468e5080e800dd2f8
FungluiKoo <fenglei.gu@nyu.edu>
2022-09-28 15:04:33 -0400

    Update 932-beautiful-array.cpp

--- 932-beautiful-array.cpp
+++ 932-beautiful-array.cpp
@@ -39,13 +39,43 @@ private:
     vector<int> arr;
     void helper(int left, int right, int a1, int an){
         int n = right-left+1;
-        if(n<=2){
+        if(n==5){
+            int d = (an-a1)>>2;
+            arr[left] = a1+(d<<1);
+            arr[left+1] = an;
+            arr[left+2] = a1;
+            arr[left+3] = a1+d;
+            arr[right] = an-d;
+            return;
+        }
+        if(n==4){
+            int d = (an-a1)/3;
             arr[left] = a1;
-            if(n==2){arr[right] = an;}
+            arr[left+1] = an-d;
+            arr[left+2] = a1+d;
+            arr[right] = an;
+            return;
+        }
+        if(n<=3){
+            switch (n)
+            {
+            case 3:
+                arr[left] = an;
+                arr[left+1] = a1;
+                arr[right] = (a1+an)>>1;
+                break;
+            case 2:
+                arr[right] = an;
+            case 1:
+                arr[left] = a1;
+                break;
+            default:
+                break;
+            }
             return;
         }
         int d = (an-a1)/(right-left);
-        int mid = (left+right+1)/2;
+        int mid = (left+right+1)>>1;
         if(n%2){ // odd
             arr[mid] = a1;
         }
