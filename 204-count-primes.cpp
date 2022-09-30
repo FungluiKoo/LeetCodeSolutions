67c17a7cdcc6aad83b10f0b425d11cd28aeebc79
FungluiKoo <fenglei.gu@nyu.edu>
2022-09-29 20:38:07 -0400

    Create 204-count-primes.cpp

---
+++ 204-count-primes.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n<=2){return 0;}
+        vector<bool> isPrime(n, true);
+        int cnt = 1;
+        int i=3;
+        int sq = ceil(sqrt(n));
+        for(; i<=sq; i+=2){
+            if(isPrime[i]){
+                ++cnt;
+                for(int x=i*3; x<n; x+=i){
+                    isPrime[x] = false;
+                }
+            }
+        }
+        for(; i<n; i+=2){
+            if(isPrime[i]){
+                ++cnt;
+            }
+        }
+        return cnt;
+    }
+};
