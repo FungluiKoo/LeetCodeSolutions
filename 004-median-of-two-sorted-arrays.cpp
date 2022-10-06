ef7ddc53efeae97b752dfdb4e6fe2ffe253c4266
FungluiKoo <fenglei.gu@nyu.edu>
2022-10-05 22:06:15 -0400

    Update 004-median-of-two-sorted-arrays.cpp

--- 004-median-of-two-sorted-arrays.cpp
+++ 004-median-of-two-sorted-arrays.cpp
@@ -10,24 +10,20 @@ public:
         int n1 = nums1.size(), n2=nums2.size();
         if(n1==0){return nums2[k-1];}
         if(n2==0){return nums1[k-1];}
-        // if(nums1[n1-1]<=nums2[0]){
-        //     return (k<=n1) ? nums1[k-1] : nums2[k-n1-1]; 
-        // }        
+     
         if(upper_bound(nums1.cbegin(),nums1.cend(),nums2[0])-nums1.cbegin()>=k){return nums1[k-1];}
-        if(lower_bound(nums2.cbegin(),nums2.cend(),nums1[n1-1])-nums2.cbegin()<k-n1){return nums2[k-n1-1];}
-        // if(nums2[n2-1]<=nums1[0]){
-        //     return (k<=n2) ? nums2[k-1] : nums1[k-n2-1];
-        // }
+        if(lower_bound(nums2.cbegin(),nums2.cend(),nums1[n1-1])-nums2.cbegin()<=k-n1){return max(nums1[n1-1],nums2[k-n1-1]);}
+
         if(upper_bound(nums2.cbegin(),nums2.cend(),nums1[0])-nums2.cbegin()>=k){return nums2[k-1];}
-        if(lower_bound(nums1.cbegin(),nums1.cend(),nums2[n2-1])-nums1.cbegin()<k-n2){return nums1[k-n2-1];}
+        if(lower_bound(nums1.cbegin(),nums1.cend(),nums2[n2-1])-nums1.cbegin()<=k-n2){return max(nums2[n2-1],nums1[k-n2-1]);}
         
         // now the answer must be in the overlapped part
-        int left=max(1,k-n2), right=min(k-1,n1);
+        int left=max(1,k-n2+1), right=min(k,n1);
         int x = (left+right)/2;
-        while(left<right){
-            if(nums2[k-x-1]>nums1[x]){ // x too small
-                left = (left==right-1) ? (left+1) : x;
-            }else if(k-x<n2 && nums1[x-1]>nums2[k-x]){ // x too large
+        while(left<right-1){
+            if(nums1[x]<nums2[k-x-1]){ // x too small
+                left = x;
+            }else if(nums1[x-1]>nums2[k-x]){ // x too large
                 right = x;
             }else{
                 break;
