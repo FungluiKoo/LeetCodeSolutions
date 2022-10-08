52bd4d15544fd2a25de53045236babc5ba860777
FungluiKoo <fenglei.gu@nyu.edu>
2022-10-08 13:30:17 -0400

    Create 136-single-number.cpp

---
+++ 136-single-number.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        for(auto num: nums){
+            result ^= num;
+        }
+        return result;
+    }
+};
