79ce4a45fc113bc5d84e96e11b8ac0002198bbfd
FungluiKoo <fenglei.gu@nyu.edu>
2022-10-08 13:07:41 -0400

    Update 461-hamming-distance.cpp

--- 461-hamming-distance.cpp
+++ 461-hamming-distance.cpp
@@ -5,7 +5,7 @@ public:
     int hammingDistance(int x, int y) {
         int a = x^y, result = 0;
         while(a){
-            if(a&0b1){++result;}
+            result += (a&0b1);
             a >>= 1;
         }
         return result;
