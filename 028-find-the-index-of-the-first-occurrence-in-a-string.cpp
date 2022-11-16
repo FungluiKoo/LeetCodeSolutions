6d54961e4311f2727080175cf5a8561f8935336e
FungluiKoo <fenglei.gu@nyu.edu>
2022-11-16 01:24:29 -0500

    Create 028-find-the-index-of-the-first-occurrence-in-a-string.cpp

---
+++ 028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,18 @@
+#include <string>
+
+using namespace std;
+
+class Solution {
+    // KMP algorithm
+public:
+    int strStr(string haystack, string needle) {
+        
+    }
+};
+
+class Solution2 {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle);
+    }
+};
