32f7a390ff98f183620244b2063beb073b5b5829
FungluiKoo <fenglei.gu@nyu.edu>
2022-11-25 16:52:46 -0500

    Update 101-symmetric-tree.cpp

--- 101-symmetric-tree.cpp
+++ 101-symmetric-tree.cpp
@@ -15,6 +15,32 @@ class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         if(root==nullptr){return true;}
+        stack<TreeNode*> S;
+        S.push(root);
+        S.push(root);
+        while(!S.empty()){
+            auto l = S.top();
+            S.pop();
+            auto r = S.top();
+            S.pop();
+            if((l->val != r->val) || ((l->left==nullptr) ^ (r->right==nullptr)) || ((l->right==nullptr)^(r->left==nullptr))){return false;}
+            if(l->right){
+                S.push(r->left);
+                S.push(l->right);
+            }
+            if(l->left){
+                S.push(r->right);
+                S.push(l->left);
+            }
+        }
+        return true;
+    }
+};
+
+class Solution2 {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(root==nullptr){return true;}
         stack<TreeNode*> SL, SR;
         SL.push(root);
         SR.push(root);
