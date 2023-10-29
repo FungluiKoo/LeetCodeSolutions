94d34bdb98de89fec09dbb194c8431da2a0b6919
FungluiKoo <fenglei.gu@nyu.edu>
2023-10-28 23:41:32 -0400

    Update 1356-sort-integers-by-the-number-of-1-bits.cc

--- 1356-sort-integers-by-the-number-of-1-bits.cc
+++ 1356-sort-integers-by-the-number-of-1-bits.cc
@@ -8,17 +8,17 @@ public:
     vector<int> sortByBits(vector<int>& arr) {
         vector<pair<int,int>> repres;
         for (auto x : arr) {
-            int cnt = 0;
+            int cnt = 0, num = x;
             while(x) {
                 cnt += x & 1;
                 x >>= 1;
             }
-            repres.push_back({cnt, x});
+            repres.push_back({cnt, num});
         }
         sort(repres.begin(), repres.end());
         vector<int> res;
-        for (auto [cnt, x] : repres) {
-            res.push_back(x);
+        for (auto [cnt, num] : repres) {
+            res.push_back(num);
         }
         return res;
     }
